faba9c2a0b59db3fa0a3989571cfc7be2be238d7
Gilberto <gilberto.aristizabal@mail.utoronto.ca>
2017-08-09 22:59:23 -0400

    trying to test if hs works, need to add fake data some how :/

--- simulator.py
+++ simulator.py
@@ -58,55 +58,66 @@ class StockSimulator():
         sleeptime = 60 - datetime.utcnow().second
         time.sleep(sleeptime)
 
-    def match_head_shoulders_helper(head_shoulder_df):
+    def match_helper(self, data, indices):
         '''
         This function returns wether:
-            1) E1 and E5 are within 1.5 percent of their average
+            1) E3 > E1 and E3 > E5
+            and
+            2) E1 and E5 are within 1.5 percent of their average
             and 
-            2) E2 and E4 are within 1.5 percent of their average
+            3) E2 and E4 are within 1.5 percent of their average
 
-        E1...E5 are the 3 highest and 2 lowest data points
+        E1...E5 are the last 5 local max/min point
         '''
-        # E1 and E5 are within 1.5 percent of their average
-        e1_e5_avg = (head_shoulder_df[1] + head_shoulder_df[5]) / 2
+        e1 = data[indices[0]]
+        e2 = data[indices[1]]
+        e3 = data[indices[2]]
+        e4 = data[indices[3]]
+        e5 = data[indices[4]]
+        # req 1: E3>E1,E3>E
+        req_1 = e3 > e1 and e3 > e5
+        
+        # req 2: E1 and E5 are within 1.5 percent of their average
+        e1_e5_avg = (e1 + e5) / 2
         percent = e1_e5_avg * 0.015
         lower_bound = e1_e5_avg - percent
-        higher_bound = e1_e5_avg + percent
+        upper_bound = e1_e5_avg + percent
             # E1 and E5 are >= lower bound
-        e1_lower = lower_bound <= head_shoulder_df[1]
-        e5_lower = lower_bound <= head_shoulder_df[5]
+        e1_lower = lower_bound <= e1
+        e5_lower = lower_bound <= e5
             # E1 and E5 are <= upper bound
-        e1_upper = upper_bound >= head_shoulder_df[1]
-        e5_upper = upper_bound >= head_shoulder_df[5]
-        if e1_lower and e5_lower and e1_upper and e5_upper:
-            # E2 and E4 are within 1.5 percent of their average
-            e2_e4_avg = (head_shoulder_df[2] + head_shoulder_df[4]) / 2
-            percent_2 = e2_e4_avg * 0.015
-            lb = e2_e4_avg - percent_2
-            ub = e2_e4_avg + percent_2
-                # E2 and E4 are >= lower bound
-            e2_lower = lb <= head_shoulder_df[2]
-            e4_lower = lb <= head_shoulder_df[4]
-                # E2 and E4 are >= lower bound
-            e2_upper = ub >= head_shoulder_df[2]
-            e4_upper = ub >= head_shoulder_df[4]
-            return e2_lower and e4_lower and e2_upper and e4_upper
-        return False
+        e1_upper = upper_bound >= e1
+        e5_upper = upper_bound >= e5
 
-    def match_head_shoulders(self, ticker):
+        req_2 = e1_lower and e5_lower and e1_upper and e5_upper
+        
+        # req 3: E2 and E4 are within 1.5 percent of their average
+        e2_e4_avg = (e2 + e4) / 2
+        percent_2 = e2_e4_avg * 0.015
+        lb = e2_e4_avg - percent_2
+        ub = e2_e4_avg + percent_2
+            # E2 and E4 are >= lower bound
+        e2_lower = lb <= e2
+        e4_lower = lb <= e4
+            # E2 and E4 are >= lower bound
+        e2_upper = ub >= e2
+        e4_upper = ub >= e4
+
+        req_3 = e2_lower and e4_lower and e2_upper and e4_upper
+            
+        return req_1 and req_2 and req_3
+
+    def match_head_shoulders(self, ticker, data):
         for ticker_data in self.ticker_data_list:
             if ticker_data[0] == ticker:
-                # From moving average, get the 3 highest and the 2 lowest points
-                mov_avg = self.get_ticker_moving_average(ticker)
-                ups = mov_avg.nlargest(3)
-                downs = mov_avg.nsmallest(2)
-                head_shoulder = pd.concat([ups, downs])
-                head_shoulder_s = head_shoulder.sort_index()
-                ## Based on this link: https://quant.stackexchange.com/questions/1937/how-to-identify-technical-analysis-chart-patterns-algorithmically
-                if head_shoulder_s[1] in ups and head_shoulder_s[3] > head_shoulder_s[1] and head_shoulder_s[3] > head_shoulder_s[5]:
-                    return self.match_head_shoulders_helper(head_shoulder_s)
-                else:
-                    return False
+                #data = self.get_ticker_moving_average(ticker).values
+                # order is the amount of data around a point to determine if is a local max/min
+                max_idx = list(argrelextrema(data, np.greater, order=1)[0])
+                min_idx = list(argrelextrema(data, np.less, order=1)[0])
+                all_idx = max_idx + min_idx
+                all_idx.sort()
+                all_idx = all_idx[-5:]
+                return self.match_helper(data, all_idx)
         raise ValueError("Ticker not tracked")
         
 def main():
@@ -120,6 +131,7 @@ def main():
     ss.print_portfolio()
     # TESTING
     ma = ss.get_ticker_moving_average('TSLA')
+    print(ma)
     ma[0] = 300
     ma[1] = 310
     ma[2] = 320
@@ -140,11 +152,17 @@ def main():
     ma[17] = 340
     ma[18] = 332
     ma[19] = 305
-    ibm = ss.get_ticker('IBM')
-    my_t = ['myt', ma]
-    print(ibm)
-    print(my_t)
-    print(ss.match_head_shoulders(my_t))
+
+    ibm_data = ss.get_ticker('IBM')
+    ibm_data[1]['HL-MovAvg'] = ma.values
+    ss.ticker_data_list.append(['MYSTK', ibm_data[1]])
+    data = ibm_data[1]['HL-MovAvg']
+    ss.match_head_shoulders('MYSTK', data)
+            
+
+    
+    
+
     
 
 if __name__ == "__main__":
